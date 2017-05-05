089272ca5505475dcf4944fa733be8ec84ff81ee
lor104 <lauren.rosentzveig@gmail.com>
2017-05-05 17:06:23 -0400

    write associations for models

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,6 @@
 class Beer < ApplicationRecord
+  has_many :bars
+  has_many :user_beers
+  belongs_to :category
+  has_many :users, through :user_beers
 end
--- app/models/user_beer.rb
+++ app/models/user_beer.rb
@@ -1,2 +1,4 @@
 class UserBeer < ApplicationRecord
+  belongs_to :user
+  belongs_to :beer
 end
