11f616d9765add027c79e43b3a41b4ec9b3297a2
Mark <markjconlon@gmail.com>
2017-05-07 15:13:36 -0400

    after user signs up they are also logged in and redirected to user_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_session_path
+      session[:user_id] = @user.id
+      redirect_to user_path("current"), notice: "Logged in!"
     else
       render :new
     end
