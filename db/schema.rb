4ea3757bc50a257085041bea1ddff10ff20aa5cd
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-07 19:50:01 -0400

    Change categories column name from type to category_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170507225404) do
+ActiveRecord::Schema.define(version: 20170507234512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 20170507225404) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string   "type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "category_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "user_beers", force: :cascade do |t|
