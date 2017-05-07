e05620273939260813152db9b5b7696d1b783711
Mark <markjconlon@gmail.com>
2017-05-07 12:21:05 -0400

    removed temporary beers views as to not conflict with other group members, changed from sessions to session in application controller as it was pluarilize by mistake, to get current user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find(sessions[:user_id]) if sessions[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   helper_method :current_user
