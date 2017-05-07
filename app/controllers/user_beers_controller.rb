aade5dbb5f26ba657aa8b45b292958c1183b6184
Mark <markjconlon@gmail.com>
2017-05-07 19:55:57 -0400

    added user_beers controller with a show view

---
+++ app/controllers/user_beers_controller.rb
@@ -0,0 +1,4 @@
+class UserBeersController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user_beers/show'
+
   resources :users, only: [:create, :new, :edit, :update, :show]
   resources :beers, only: [:index, :show]
 
