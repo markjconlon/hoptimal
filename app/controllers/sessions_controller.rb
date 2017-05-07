0fb46e46bf2a46a91fb07f97e1acb02a394016ad
Mark <markjconlon@gmail.com>
2017-05-07 14:24:42 -0400

    can log in with a session redirects to beers index page atm

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
+      puts "WORKED"
       session[:user_id] = user.id
-      redirect_to user_path, :notice => "Logged in!"
+      redirect_to beers_path, notice: "Logged in!"
     else
       flash.now[:alert] = "Invalid username or password"
       render "new"
@@ -15,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to new_session_path_url, notice: "Logged out!"
+    redirect_to new_session_path, notice: "Logged out!"
   end
 end
