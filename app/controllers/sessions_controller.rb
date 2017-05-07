c36a27224e5c09d950494ea483b5768f9edcf09d
Mark <markjconlon@gmail.com>
2017-05-07 13:24:29 -0400

    work in progress users landing page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to users_path, :notice => "Logged in!"
+      redirect_to user_path, :notice => "Logged in!"
     else
-      flash.now[:alert] = "invalid username or password"
+      flash.now[:alert] = "Invalid username or password"
       render "new"
     end
   end
 
   def destroy
-    sessions[:user_id] = nil
-    redirect_to new_session_url, :notice => "Logged out!"
+    session[:user_id] = nil
+    redirect_to new_session_path_url, notice: "Logged out!"
   end
 end
