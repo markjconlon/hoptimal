5ff9c50fd94cbb1098d22f5f4fde9da60a93a6b1
Mark <markjconlon@gmail.com>
2017-05-07 12:05:42 -0400

    commented out turbo links from application JS, made views for sessions new and users new as well as show, added mising : to many associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :new, :edit, :update]
+  resources :users, only: [:create, :new, :edit, :update, :show]
   resources :beers, only: [:index, :show]
 
   resources :sessions, only: [:new, :create, :destroy]
