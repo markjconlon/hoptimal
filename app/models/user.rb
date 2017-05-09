1f5492f4b2e45c047985944103af9f1b41d5a0d2
Mark <markjconlon@gmail.com>
2017-05-09 12:58:39 -0400

    random_by_previous returns one suggestion based on previous user_beers, has some duplication with random_by_preference, will refactor at a later date also currently rating is set to nil will want to change to 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,7 +50,22 @@ class User < ApplicationRecord
 
   def random_by_previous(current_user)
     rating_3_or_above = UserBeer.where(user_id: current_user, rating: nil)
-    return Beer.where(id: (rating_3_or_above.last).beer_id)
+    beer_id_for_category = rating_3_or_above.sample.beer_id
+    category_to_pull_from = Beer.find(beer_id_for_category).category_id
+
+    had_before = current_user.user_beers.all
+    had_before_ids = only_ids(had_before)
+
+    all_beers_of_type = Beer.where(category_id: category_to_pull_from)
+
+    not_had_before = []
+    all_beers_of_type.each do |beer|
+      if !(had_before_ids.include?(beer.id))
+        not_had_before << beer
+      end
+    end
+
+    return not_had_before.sample
   end
 
   def most_recent_4(current_user)
