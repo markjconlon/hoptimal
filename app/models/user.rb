e1fc7e652b0bf0af9468640826485c55e43f0279
Mark <markjconlon@gmail.com>
2017-05-13 12:31:34 -0400

    refactored only_ids method to one line in collection_to_sample_from method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,24 +11,16 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 
-  def only_ids(had_before)
-    beer_ids = []
-    had_before.each do |beer|
-      beer_ids << beer.beer_id
-    end
-    beer_ids
-  end
 
   def collection_to_sample_from(current_user, all_beers_of_type)
     had_before = current_user.user_beers.all
-    had_before_ids = only_ids(had_before)
+    had_before_ids = had_before.map{|beer| beer.beer_id}
     not_had_before = []
     all_beers_of_type.each do |beer|
       if !(had_before_ids.include?(beer.id))
         not_had_before << beer
       end
     end
-
     return not_had_before
   end
 
@@ -47,6 +39,13 @@ class User < ApplicationRecord
       beer_id_for_category = rating_3_or_above.sample.beer_id
       category_to_pull_from = Beer.find(beer_id_for_category).category_id
     end
+    # Now have to check if they have cleared the category chosen in category_to_pull_from
+    # category_cleared? = (UserBeer.where(user_id: current_user.id)
+    # if condition
+    #
+    # else
+    #
+    # end
 
     all_beers_of_type = Beer.where(category_id: category_to_pull_from)
 
