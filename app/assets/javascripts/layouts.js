88a79d958b91bbf3e8b326affd04c7b356579c30
lor104 <lauren.rosentzveig@gmail.com>
2017-05-16 22:29:34 -0400

    add modal window for signup, have either modal window fadeout with the esc keypress

--- app/assets/javascripts/layouts.js
+++ app/assets/javascripts/layouts.js
@@ -1,22 +1,47 @@
 $(document).ready(function() {
 
   var login = $('.logged-out-nav a:first');
-  var signup = $('.logged-out-nav a:last')
+  var signup = $('.logged-out-nav a:last');
+  modalLogin = $('.modal-login')
+  modalSignup = $('.modal-signup')
 
   $(login).on('click', function(e) {
     // e.stopPropogation();
     e.preventDefault();
-    console.log('click login');
-    $('.modal-login').fadeIn();
+    modalLogin.fadeIn();
   });
 
   $('.users-form').on('click', function(e) {
     e.stopPropagation();
   })
 
-  $('.modal-login').on('click', function(e) {
+  modalLogin.on('click', function(e) {
     $(this).fadeOut();
   });
 
+  $(signup).on('click', function(e) {
+    // e.stopPropogation();
+    e.preventDefault();
+    console.log('click signup');
+    modalSignup.fadeIn();
+  });
+
+  $('.users-form').on('click', function(e) {
+    e.stopPropagation();
+  })
+
+  modalSignup.on('click', function(e) {
+    $(this).fadeOut();
+  });
+
+  $(document).keyup(function(event){
+      if(event.which=='27'){
+        modalLogin.fadeOut();
+        modalSignup.fadeOut();
+      }
+    });
+
+
+    })
 
 });
