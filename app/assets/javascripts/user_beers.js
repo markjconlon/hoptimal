a6161f568b3e51dcbfd81c97208cf19e3bed20a0
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-17 21:32:12 -0400

    add ajax request for search in user_beers

--- app/assets/javascripts/user_beers.js
+++ app/assets/javascripts/user_beers.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+
+// Filters beers index page based on user input typed within the search field
+  $("#search-field").on('keyup', function(e){
+    $.ajax({
+      url: "/user_beers",
+      method: "POST",
+      data: {
+        "q": {
+          "name_cont": $("#search-field").val()
+        }
+      },
+      dataType: 'html'
+    }).done(function(data) {
+        });
+      });
+    })
+  });
+})
