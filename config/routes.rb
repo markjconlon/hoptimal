e68244a11c71146d057d2859ee6ecd2efa7c52c5
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-17 20:59:56 -0400

    add post route to user_beers

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root 'beers#index'
   get 'user_beers/show'
+  post 'user_beers/show' => 'user_beers#show'
 
   # random beers route added to allow for json requests for random preference
   get '/beers/random/preference' => 'beers#random_by_preference'
 
   # random beers route added to allow for json requests for un had random beers
   get '/beers/random' => 'beers#random'
-  # link added to allow for json request for random beer by suggestion :STRECTHER GOAL 
+  # link added to allow for json request for random beer by suggestion :STRECTHER GOAL
   get '/beers/suggestion' => 'beers#suggestion'
 
 
